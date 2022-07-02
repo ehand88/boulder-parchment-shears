41dda3ac0be2150fb80627dc53c8fabda1312b00
Eric Hand <ethehand@gmail.com>
2022-07-02 15:37:42 -0700

    Initialize styles.css and main.js, html header

---
+++ main.js
@@ -0,0 +1,8 @@
+//  get input from the user
+
+//  get random number, 1-3, for computer
+
+//  compare inputs to determine winner of round
+
+/*  add round result to results table, 
+    if number of wins = 5 then declare winner, else continue */
