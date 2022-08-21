d63608b96df322c0884656aade1e125a0245ff83
Eric Hand <ethehand@gmail.com>
2022-08-20 21:10:39 -0700

    Minimize UI and clean up early code

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // declare global vars (bad practice, but I struggled to think of a better way)
 const SCORE_TO_WIN = 5;
 const bpsButtons = document.querySelectorAll('.bps-button');
+const startButton = document.querySelector('.btn-start');
+const output = document.querySelector('.output');
 let playerScore = 0;
 let computerScore = 0;
 let round = 0;
@@ -11,12 +13,14 @@ let roundResults = [];
 function playGame() {
     playerScore = 0;
     computerScore = 0;
-    clearStyle('.btn-start','active');
+    startButton.parentElement.classList.remove('active');
+    startButton.parentElement.classList.add('disabled');
     document.querySelector('.score-board > .middle-column').innerText = '';
     updateScore('player', 0);
     updateScore('computer', 0);
+    output.innerText = 'Make your selection!';
 
-// Determine which input the user clicks
+    // handle user input
     bpsButtons.forEach(button => {
         if(button.id !== 'computer') {
             button.removeAttribute('disabled','');
@@ -33,7 +37,7 @@ function playRound() {
     const computerPlay = determineComputerPlay();
     updateSelection('computer', computerPlay);
 
-// populate round results
+    // populate round results
     const roundResult = determineWinner(playerPlay, computerPlay);
     playerScore += (roundResult === 'player') ? 1 : 0;
     computerScore += (roundResult === 'computer') ? 1 : 0;
@@ -41,34 +45,32 @@ function playRound() {
     computerResults.unshift([computerPlay, computerScore]);
     roundResults.unshift(roundResult);
 
-    // stylize boards
+    // stylize board and display winner text
     if(roundResult !== 'tie') {
         stylizeWinnerBoard(button, roundResult);
+        output.innerText = `${roundResult.toUpperCase()} wins the round!`;
+    } else {
+        output.innerText = `${roundResult.toUpperCase()}!`;        
     }
 
     //animate boards
     animateSelections(button);
     setTimeout(() => {
         animateSelections(button);
-        clearStyle('.bps-button', 'winner');
-    }, 1500);
+        clearStyle('.bps-button', 'highlight');
+        output.innerText = 'Make your selection!';
 
-// calculate stats - total rounds; total r,b,s; ratio r,b,s; wins/losses
+        // determine if game winner decided yet
+        if(playerScore === SCORE_TO_WIN || computerScore === SCORE_TO_WIN) {
+            (playerScore === SCORE_TO_WIN) ? declareWinner('player') : declareWinner('computer');
+        }
+    }, 1200);
 
-// populate boards
+    // populate boards
     updateScore('player', playerScore);
     updateScore('computer', computerScore);
     updateResults(playerResults[0], computerResults[0], roundResults[0]);
 
-// determine if game winner decided yet
-    if(playerScore === SCORE_TO_WIN || computerScore === SCORE_TO_WIN) {
-        (playerScore === SCORE_TO_WIN) ? declareWinner('player') : declareWinner('computer');
-
-
-
-        // drawChart('player', playerResults);
-        // drawChart('computer', computerResults);
-    }
     round++;
 }
 
@@ -82,71 +84,30 @@ function animateSelections(clicked) {
     });
 }
 
-function drawChart(entity, entityData) {;
-    let xValues = ['boulder', 'parchment', 'shears'];
-    let yValues = [0, 0, 0];
-    for(let i = 0; i < entityData.length; i++) {
-        switch (entityData[i][0]) {
-            case 'boulder':
-                yValues[0]++;
-                break;
-            case 'parchment':
-                yValues[1]++;
-                break;
-            case 'shears':
-                yValues[2]++;
-                break;
-        }
-    }
-    let barColors = [
-    "#563f46",
-    "#8ca3a3",
-    "#484f4f"
-    ];
-
-    new Chart(`${entity}Pie`, {
-    type: "pie",
-    data: {
-        labels: xValues,
-        datasets: [{
-        backgroundColor: barColors,
-        data: yValues
-        }]
-    },
-    options: {
-        title: {
-        display: true,
-        responsive: true,
-        maintainAspectRatio: false,
-        text: "BPS Selection"
-        }
-    }
-    });
-}
-
 function declareWinner(winner) {
-    document.querySelector('.score-board > .middle-column').innerText = `${winner.toUpperCase()} wins!`;
-    const startButton = document.querySelector('.btn-start')
+    output.innerText = `${winner.toUpperCase()} wins the game!`;
     
     if(winner === 'player') {
-        startButton.innerText = 'Defend Your Crown!';
+        startButton.innerText = 'Defend Your Title!';
     } else {
         startButton.innerText = 'Reclaim Your Honor!';
     }
 
-    startButton.classList.add('active');
-
-    // remove event listener
+    startButton.parentElement.classList.add('active');
+    startButton.parentElement.classList.remove('disabled');
+    
+    // remove event listener & hide buttons
     bpsButtons.forEach(btn => {
         btn.removeEventListener('click', playRound);
         btn.setAttribute('disabled','');
-        btn.classList.remove('active');
+        btn.parentElement.classList.remove('active');
+        btn.parentElement.classList.add('disabled');
     });
 }
 
 function stylizeWinnerBoard(board, winner) {
     const winnerBoard = (winner === 'player') ? board : document.querySelector(`.${winner} > .bps-button`);
-    winnerBoard.classList.add('winner');
+    winnerBoard.classList.add('highlight');
 }
 
 function clearStyle(query,classToRemove) {
@@ -164,13 +125,7 @@ function updateSelection(entity, entitySelection) {
 }
 
 function updateResults(playerData, computerData, resultData) {
-    const resultsTable = document.querySelector('.results-table > tbody');
-// 
-    if(resultsTable.parentElement.clientHeight + 20 > resultsTable.parentElement.parentElement.clientHeight) {
-        const tRows = document.querySelectorAll('tr');
-        resultsTable.removeChild(tRows[tRows.length - 1]);
-    }
-    
+    const resultsTable = document.querySelector('.results-table > tbody');    
     const tRow = document.createElement('tr');
     const tdPlayerPlay = document.createElement('td');
     const tdPlayerScore = document.createElement('td');
@@ -204,7 +159,7 @@ function updateResults(playerData, computerData, resultData) {
 }
 
 
-//  get random number, 1-3, for computer
+//  determine computer play based on random number, 1-3
 function determineComputerPlay() {
     let randInt = Math.floor(Math.random()*3) +1;
     switch (randInt) {
@@ -239,16 +194,4 @@ function determineWinner(playerChoice,computerChoice) {
     }
     return result;
 }
-
-/* just use css :hover instead
-document.querySelectorAll('.bpsButton').forEach((bps) => bps.addEventListener('pointerenter', function(e) {
-    this.classList.add('grow');
-}));
-
-document.querySelectorAll('.bpsButton').forEach((bps) => bps.addEventListener('pointerleave', function(e) {
-    this.classList.remove('grow');
-}));
-*/
-
-// Function to display results
 
